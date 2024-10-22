c81b4a3fc8915dd9154e6a870413ccd5bf89f42e
Nico Benito <Nico.Benito@lanrex.com.au>
2024-10-23 06:04:16 +0800

    Updated code for computation of appeal and packages

---
+++ Entities/Integration Data Model/CSVParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace Fundraising_Engagement.Plugins.Entities.Integration_Data_Model
+{
+    public class CSVParser
+    {
+        public List<DonationModel> ParseDonationRecords(string csvFilePath)
+        {
+            try
+            {
+                using (var reader = new StreamReader(csvFilePath))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    // Set up CSV configuration here if necessary (e.g., Delimiter, IgnoreBlankLines, etc.)
+                    var records = csv.GetRecords<DonationModel>();
+                    return new List<DonationModel>(records);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any parsing exceptions (e.g., log or rethrow)
+                Console.WriteLine($"Error occurred while parsing: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
