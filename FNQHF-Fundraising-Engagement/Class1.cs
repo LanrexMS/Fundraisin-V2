2be17f879d63f4e547f8a1a978ab117dab32f773
Nico Benito <Nico.Benito@lanrex.com.au>
2024-11-11 12:41:50 +0800

    FNQHF project added

---
+++ FNQHF-Fundraising-Engagement/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNQHF_Fundraising_Engagement
+{
+    public class Class1
+    {
+    }
+}
